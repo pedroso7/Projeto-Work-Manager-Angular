f8d7d9ce7765af4439c3af1fcbbea4af0294e981
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-10 23:43:16 +0000

    Criação do componente tela

--- work-manager/src/app/cliente/cliente-module.ts
+++ work-manager/src/app/cliente/cliente-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClienteRoutingModule } from './cliente-routing-module';
+import { Tela } from './tela/tela';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Tela
+  ],
   imports: [
     CommonModule,
     ClienteRoutingModule
