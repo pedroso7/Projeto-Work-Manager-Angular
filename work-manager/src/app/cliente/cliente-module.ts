3345005c6f9a7d6990716a29b370e732b578bb99
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-10 23:44:35 +0000

    Criação do componente cadastro

--- work-manager/src/app/cliente/cliente-module.ts
+++ work-manager/src/app/cliente/cliente-module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ClienteRoutingModule } from './cliente-routing-module';
 import { Tela } from './tela/tela';
+import { Cadastro } from './cadastro/cadastro';
 
 
 @NgModule({
   declarations: [
-    Tela
+    Tela,
+    Cadastro
   ],
   imports: [
     CommonModule,
