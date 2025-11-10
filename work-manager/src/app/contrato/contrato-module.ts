6bc0215f2adc6bde161c2b19b64ae8c377e20015
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-10 23:41:11 +0000

    Criação do módulo contrato

---
+++ work-manager/src/app/contrato/contrato-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContratoRoutingModule } from './contrato-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContratoRoutingModule
+  ]
+})
+export class ContratoModule { }
