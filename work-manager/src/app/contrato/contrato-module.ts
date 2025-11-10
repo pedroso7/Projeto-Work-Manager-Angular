e51183603ffbf18f39752a997cdfba14b806a020
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-10 23:46:02 +0000

    Criação do componente tela no módulo contrato

--- work-manager/src/app/contrato/contrato-module.ts
+++ work-manager/src/app/contrato/contrato-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContratoRoutingModule } from './contrato-routing-module';
+import { Tela } from './tela/tela';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Tela
+  ],
   imports: [
     CommonModule,
     ContratoRoutingModule
