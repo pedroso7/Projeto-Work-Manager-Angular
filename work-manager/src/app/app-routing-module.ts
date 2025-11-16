ea47bbf52d52b4de30fbe7baa40164f90a174ec4
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-16 21:44:57 +0000

    excluir módulos para reestruturação

--- work-manager/src/app/app-routing-module.ts
+++ work-manager/src/app/app-routing-module.ts
@@ -2,14 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'cliente',
-    loadChildren: () => import('./cliente/cliente-module').then(m => m.ClienteModule)
-  },
-  {
-    path: 'contrato',
-    loadChildren: () => import('./contrato/contrato-module').then(m => m.ContratoModule)
-  }
+
 ];
 
 @NgModule({
