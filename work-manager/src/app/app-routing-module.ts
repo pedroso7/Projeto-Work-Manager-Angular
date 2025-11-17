51ea460ca4e29c0fc1cf8afdc53a9ccb6dfb83e9
pedroso7 <viniciuspgarcia1@gmail.com>
2025-11-17 01:17:41 +0000

    Configurando rotas

--- work-manager/src/app/app-routing-module.ts
+++ work-manager/src/app/app-routing-module.ts
@@ -2,6 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'prestador',
+    loadChildren: () => import('./prestador/prestador-module').then(m => m.PrestadorModule)
+  },
+  {
+    path: 'servicos',
+    loadChildren: () => import('./servicos/servicos-module').then(m => m.ServicosModule)
+  },
+  {
+    path: '', redirectTo: 'servicos', pathMatch: 'full'
+  },
+ 
 
 ];
 
--- work-manager/src/app/servicos/servicos-routing-module.ts
+++ work-manager/src/app/servicos/servicos-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Editar } from './editar/editar';
+import { Listagem } from './listagem/listagem';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cadastro', component: Cadastro},
+  {path: 'editar', component: Editar},
+  {path: 'listagem', component: Listagem}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
